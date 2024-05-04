72a24e2d0fe3b02a8f44b3c26d198e26502a74c5
Player one <es.korepanov@gmail.com>
2024-05-04 21:51:38 +0400

    to lb

--- src/main/java/ru/korepanov/gamification/game/controller/LeaderBoardController.java
+++ src/main/java/ru/korepanov/gamification/game/controller/LeaderBoardController.java
@@ -2,23 +2,26 @@ package ru.korepanov.gamification.game.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.korepanov.gamification.game.service.LeaderBoardService;
 import ru.korepanov.gamification.game.domain.LeaderBoardRow;
+import ru.korepanov.gamification.game.service.LeaderBoardService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/leaders")
 @RequiredArgsConstructor
+@Slf4j
 public class LeaderBoardController {
 
     private final LeaderBoardService leaderBoardService;
 
     @GetMapping
     public List<LeaderBoardRow> getLeaderBoard() {
+        log.info("Retrieving leaderboard");
         return leaderBoardService.getCurrentLeaderBoard();
     }
 }
